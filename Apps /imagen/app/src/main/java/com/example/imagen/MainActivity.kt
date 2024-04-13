c35e84d941aa00428602c49b1ed8d196bf93c705
ELMANCHE <eliasx2x@gmail.com>
2024-04-13 18:17:49 -0500

    imagen

--- Apps /imagen/app/src/main/java/com/example/imagen/MainActivity.kt
+++ Apps /imagen/app/src/main/java/com/example/imagen/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.imagen
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Llama al método onCreate de la clase padre AppCompatActivity
+        // para inicializar la actividad.
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        // Establece el diseño de la actividad a activity_main.
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Encuentra la vista ImageView con el id imageView
+        // y la asigna a la variable potaxio.
+        val potaxio = findViewById<ImageView>(R.id.imageView)
+
+        // Asigna un OnClickListener a la ImageView potaxio.
+        potaxio.setOnClickListener{
+            // Muestra un Toast con el mensaje "Potaxio" durante un tiempo largo.
+            Toast.makeText(this,"Potaxio", Toast.LENGTH_LONG).show()
         }
     }
 }
