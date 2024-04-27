f36c4d023ec446b196c2a192a45fc76c2845aa76
ELMANCHE <eliasx2x@gmail.com>
2024-04-26 22:03:33 -0500

    avance fragments

--- Apps /Fragments/app/build.gradle.kts
+++ Apps /Fragments/app/build.gradle.kts
@@ -26,6 +26,14 @@ android {
             )
         }
     }
+
+
+    buildFeatures{
+
+        viewBinding = true
+    }
+
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -36,6 +44,7 @@ android {
 }
 
 dependencies {
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
---
+++ Apps /Fragments/app/src/main/java/com/example/fragment/segundo_fragent.kt
@@ -0,0 +1,4 @@
+package com.example.fragment
+
+class segundo_fragent {
+}
