3a828d517aefeb3dfc90f5b2ae408198779240ec
ELMANCHE <eliasx2x@gmail.com>
2024-05-11 11:51:03 -0500

    ajustando fragmentos

--- Apps /quiz/app/src/main/java/com/example/quiz/primera_pregunta.kt
+++ Apps /quiz/app/src/main/java/com/example/quiz/primera_pregunta.kt
@@ -2,13 +2,11 @@ package com.example.quiz
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 
-class Bienvenida : Fragment(R.layout.fragment_bienvenida) {
 
+class primera_pregunta : Fragment(R.layout.fragment_primera_pregunta) {
+    
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
