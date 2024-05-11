5948fedbae6effd3cbaa5b31ace206072c8e4b58
ELMANCHE <eliasx2x@gmail.com>
2024-05-11 12:15:09 -0500

    Update primera_pregunta.kt

--- Apps /quiz/app/src/main/java/com/example/quiz/primera_pregunta.kt
+++ Apps /quiz/app/src/main/java/com/example/quiz/primera_pregunta.kt
@@ -6,7 +6,7 @@ import androidx.fragment.app.Fragment
 
 
 class primera_pregunta : Fragment(R.layout.fragment_primera_pregunta) {
-    
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
