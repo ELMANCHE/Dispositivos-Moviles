7975f12de3cd33eb9955df37f24b316753653cde
ELMANCHE <eliasx2x@gmail.com>
2024-05-11 18:11:42 -0500

    corrigiendo errores y agregando mas funcionalidades
    por ahora todo funciona

--- Apps /quiz/app/src/main/java/com/example/quiz/MainActivity.kt
+++ Apps /quiz/app/src/main/java/com/example/quiz/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.quiz
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.quiz.databinding.ActivityMainBinding
+
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
     }
 }
