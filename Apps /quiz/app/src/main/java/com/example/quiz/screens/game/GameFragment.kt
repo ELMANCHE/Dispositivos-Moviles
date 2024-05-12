ffd66c3b0b5c6e23efffb09146346f0946d56b87
ELMANCHE <eliasx2x@gmail.com>
2024-05-11 20:47:43 -0500

    agregando mas funciones para el quiz
    rastreando la respuesta correcta, el progreso de como avanza el juego, agregando metodo que se haga cargo del texto de la pregunta , actualizando el boton por el estado que va andando el quiz y configurare el onclicklistener

--- Apps /quiz/app/src/main/java/com/example/quiz/screens/game/GameFragment.kt
+++ Apps /quiz/app/src/main/java/com/example/quiz/screens/game/GameFragment.kt
@@ -1,11 +1,15 @@
 package com.example.quiz.screens.game
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.quiz.Constants
 import com.example.quiz.Question
@@ -13,9 +17,13 @@ import com.example.quiz.R
 import com.example.quiz.databinding.FragmentGameBinding
 
 
-class GameFragment : Fragment() {
+class GameFragment : Fragment(), View.OnClickListener {
     lateinit var binding: FragmentGameBinding
     lateinit var mQuestionslist: ArrayList<Question>
+
+    private  var mSelectedPosition: Int = 0
+    private var mCorrectAnswer : Int = 0
+    var mCurrrentPosition: Int = 1
 
 
 
@@ -29,14 +37,70 @@ class GameFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
 
         mQuestionslist = Constants.getQuestion()
-        for(questions in mQuestionslist){
-            Log.i("Questions", questions.question)
-        }
+
+        binding.tvOptionOne.setOnClickListener(this)
+        binding.tvOptionTwo.setOnClickListener(this)
+        binding.tvOptionThree.setOnClickListener(this)
+        binding.tvOptionFour.setOnClickListener(this)
+        binding.btnSubmit.setOnClickListener(this)
+
+        setQuestion()
 
 
         return  binding.root
 
 
+    }
+
+    private fun setQuestion() {
+
+        var question: Question = mQuestionslist[mCurrrentPosition -1]
+
+        binding.tvQuestion.text = question.question
+        binding.imageView.setImageResource(question.image)
+        binding.tvOptionOne.text = question.optionOne
+        binding.tvOptionTwo.text = question.optionTwo
+        binding.tvOptionThree.text = question.optionThree
+        binding.tvOptionFour.text = question.optionFour
+
+        binding.pb.progress = mCurrrentPosition
+        binding.tvProgress.text = "$mCurrrentPosition" + "/" + binding.pb.max
+
+
+        defauldAppearance()
+
+        if (mCurrrentPosition == mQuestionslist.size){
+
+            binding.btnSubmit.text = "Quiz Finished"
+
+        } else {
+
+            binding.btnSubmit.text = "Submit"
+        }
+    }
+
+    private fun defauldAppearance() {
+
+        val options = ArrayList<TextView>()
+        options.add(0, binding.tvOptionOne)
+        options.add(1, binding.tvOptionTwo)
+        options.add(2, binding.tvOptionThree)
+        options.add(3, binding.tvOptionFour)
+
+        for (option in options){
+
+
+            option.setTextColor(Color.parseColor("#7A8089"))
+            //default appearance
+            option.typeface = Typeface.DEFAULT
+            option.background = context?.let { ContextCompat.getDrawable(it, R.drawable.default_option_fondopotaxie) }
+
+
+        }
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
     }
 
 
