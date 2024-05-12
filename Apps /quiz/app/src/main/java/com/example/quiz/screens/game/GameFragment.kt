a924a06392b4eabfa1ad1cb8edcf9ada6180b918
ELMANCHE <eliasx2x@gmail.com>
2024-05-11 20:15:07 -0500

    implementacion de las preguntas
    en el gamefragment

--- Apps /quiz/app/src/main/java/com/example/quiz/screens/game/GameFragment.kt
+++ Apps /quiz/app/src/main/java/com/example/quiz/screens/game/GameFragment.kt
@@ -1,17 +1,22 @@
 package com.example.quiz.screens.game
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import com.example.quiz.Constants
+import com.example.quiz.Question
 import com.example.quiz.R
 import com.example.quiz.databinding.FragmentGameBinding
 
 
 class GameFragment : Fragment() {
     lateinit var binding: FragmentGameBinding
+    lateinit var mQuestionslist: ArrayList<Question>
+
 
 
     override fun onCreateView(
@@ -22,6 +27,11 @@ class GameFragment : Fragment() {
 
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
+
+        mQuestionslist = Constants.getQuestion()
+        for(questions in mQuestionslist){
+            Log.i("Questions", questions.question)
+        }
 
 
         return  binding.root
