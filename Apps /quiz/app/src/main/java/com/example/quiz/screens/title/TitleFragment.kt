21804d023dd6b68953cb55a149e7ff8e00cc8d2f
ELMANCHE <eliasx2x@gmail.com>
2024-05-11 19:28:14 -0500

    corrigiendo mis errores

--- Apps /quiz/app/src/main/java/com/example/quiz/screens/title/TitleFragment.kt
+++ Apps /quiz/app/src/main/java/com/example/quiz/screens/title/TitleFragment.kt
@@ -1,13 +1,14 @@
-package com.example.quiz.screens.title
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.findNavController
 import com.example.quiz.R
 import com.example.quiz.databinding.FragmentTitleBinding
 
@@ -17,6 +18,8 @@ import com.example.quiz.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
     lateinit var binding: FragmentTitleBinding
+    lateinit var et_name: EditText
+    lateinit var btn_start: Button
 
 
     override fun onCreateView(
@@ -27,6 +30,15 @@ class TitleFragment : Fragment() {
 
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+
+        binding.startBtn.setOnClickListener { view: View ->
+
+            if (binding.etName.text!!.isNotEmpty()) {
+                view.findNavController().navigate(R.id.action_scoreFragment_to_titleFragment)
+            } else {
+                Toast.makeText(context, "Enter your name to start", Toast.LENGTH_SHORT).show()
+            }
+        }
 
 
         return  binding.root
