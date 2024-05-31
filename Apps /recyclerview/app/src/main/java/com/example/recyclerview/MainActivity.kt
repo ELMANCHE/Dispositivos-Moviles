5eb7ce713c0d64510baf6e3c02d0def629f281b6
ELMANCHE <110752573+ELMANCHE@users.noreply.github.com>
2024-05-30 22:21:22 -0500

    creando lista
    creando lista de imagenes en el main activity

--- Apps /recyclerview/app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ Apps /recyclerview/app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -7,14 +7,59 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    val imagenFirst = listOf<potaxio>(
+        potaxio(
+            "uno",
+            "dos",
+            "tres",
+            "htttps://google.com/balbla.jpg"
+        ),
+
+        potaxio(
+            "uno",
+            "dos",
+            "tres",
+            "htttps://google.com/balbla.jpg"
+        ),
+
+        potaxio(
+            "uno",
+            "dos",
+            "tres",
+            "htttps://google.com/balbla.jpg"
+        ),
+
+        potaxio(
+            "uno",
+            "dos",
+            "tres",
+            "htttps://google.com/balbla.jpg"
+        ),
+
+        potaxio(
+            "uno",
+            "dos",
+            "tres",
+            "htttps://google.com/balbla.jpg"
+        ),
+
+        potaxio(
+            "uno",
+            "dos",
+            "tres",
+            "htttps://google.com/balbla.jpg"
+        ),
+
+        potaxio(
+            "uno",
+            "dos",
+            "tres",
+            "htttps://google.com/balbla.jpg"
+        )
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
---
+++ Apps /recyclerview/app/src/main/java/com/example/recyclerview/potaxio.kt
@@ -0,0 +1,3 @@
+package com.example.recyclerview
+
+data class potaxio (val potaxio:String, val publisher:String, val realName:String, val photo:String )
