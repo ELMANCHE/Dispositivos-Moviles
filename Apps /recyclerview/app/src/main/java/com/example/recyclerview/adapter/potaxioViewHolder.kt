af52bd0ac39ba865f736f4a220b412de7b0efdbc
ELMANCHE <110752573+ELMANCHE@users.noreply.github.com>
2024-05-31 07:11:18 -0500

    agregando funcionalidades
    generando variables para acceder al item y funcion que llame cada listado de las imagenes

--- Apps /recyclerview/app/src/main/java/com/example/recyclerview/adapter/potaxioViewHolder.kt
+++ Apps /recyclerview/app/src/main/java/com/example/recyclerview/adapter/potaxioViewHolder.kt
@@ -1,8 +1,28 @@
 package com.example.recyclerview.adapter
 
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.recyclerview.R
+import com.example.recyclerview.potaxio
 
 class potaxioViewHolder(view: View):RecyclerView.ViewHolder(view)  {
+
+    val potaxio = view.findViewById<TextView>(R.id.tvPotaxioName)
+    val realName = view.findViewById<TextView>(R.id.tvRealName)
+    val Publisher = view.findViewById<TextView>(R.id.tvPublisher)
+    val photo = view.findViewById<ImageView>(R.id.ivPotaxie)
+
+
+
+
+
+    fun render(potaxioModel: potaxio){
+        potaxio.text = potaxioModel.potaxio
+        realName.text = potaxioModel.realName
+        Publisher.text = potaxioModel.publisher
+
+    }
 }
