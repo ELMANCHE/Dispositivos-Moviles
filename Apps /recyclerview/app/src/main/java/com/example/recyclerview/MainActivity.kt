1159c7037e65284b0a3223de5ca2443031cbf501
ELMANCHE <110752573+ELMANCHE@users.noreply.github.com>
2024-05-31 07:21:56 -0500

    pruebas
    todo funciona pero aun falta mucho por mejorar

--- Apps /recyclerview/app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ Apps /recyclerview/app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerview.adapter.potaxioAdapter
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +16,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         potaxieProvaider.imageList
         setContentView(R.layout.activity_main)
+    }
+    fun initRecyclerView(){
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerPotaxie)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = potaxioAdapter(potaxieProvaider.imageList)
     }
 }
