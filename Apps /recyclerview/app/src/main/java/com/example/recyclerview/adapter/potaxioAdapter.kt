1d9eede2cbf572770d0c7890771eb93840dd5465
ELMANCHE <110752573+ELMANCHE@users.noreply.github.com>
2024-05-30 22:54:43 -0500

    agregando funcionalidades
    agregando mas funcionalidades e incluso unos tools en en itempotaxio.xml para la previa

--- Apps /recyclerview/app/src/main/java/com/example/recyclerview/adapter/potaxioAdapter.kt
+++ Apps /recyclerview/app/src/main/java/com/example/recyclerview/adapter/potaxioAdapter.kt
@@ -1,4 +1,17 @@
 package com.example.recyclerview.adapter
 
-class potaxioAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerview.potaxio
+
+class potaxioAdapter(val potaxioList: List<potaxio>): RecyclerView.Adapter<potaxioViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): potaxioViewHolder {
+
+    }
+
+    override fun getItemCount(): Int = potaxioList.size
+
+    override fun onBindViewHolder(holder: potaxioViewHolder, position: Int) {
+
+    }
 }
