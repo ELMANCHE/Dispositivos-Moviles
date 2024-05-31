f39358f52e6177040fe496b293af09cf2fa3082d
ELMANCHE <110752573+ELMANCHE@users.noreply.github.com>
2024-05-30 22:27:20 -0500

    llamando
    llamando en el main el listado

--- Apps /recyclerview/app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ Apps /recyclerview/app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        potaxieProvaider.imageList
         setContentView(R.layout.activity_main)
     }
 }
